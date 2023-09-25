d14f56c92e0da87b1f71eee50789e1bfadd434f7
Gabriel Pinheiro <45916330+gabepinheiro@users.noreply.github.com>
2023-09-25 13:05:16 -0300

    chore(setup): extend eslint config and add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,47 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
+    //disable rule of space before function parentheses
+    'space-before-function-paren': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    'no-undef': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-pascal-case': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'off',
+      {
+        ignoreRestSiblings: true,
+      },
     ],
   },
 }
